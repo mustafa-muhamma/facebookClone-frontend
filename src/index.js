b6eef73ce4e5ab893305df55d70e1ec0c7ab40ad
mustafa-muhamma <shakhsiamohamedabelmaguidgttzv@gamil.com>
2022-10-10 02:19:59 +0200

    shadow changing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { MainProvider } from './contexts/MainContext';
-import {BASE_URL} from './components/API/APIs'
+import {BASE_URL} from './components/API/APIs';
 
 axios.defaults.baseURL = BASE_URL;
 
